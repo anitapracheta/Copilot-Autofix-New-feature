af5115432ad0c0774ea8acb75facb67eac4a4c9f
anitapracheta <anita.pracheta@gmail.com>
2024-09-12 19:29:34 +0530

    Update Bank Account.py

--- Bank Account.py
+++ Bank Account.py
@@ -7,7 +7,7 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"Deposited {amount}. New balance is {self.balance}.")
+            print(f"Deposited {amount}. New balance is {self.balance.")
         else:
             print("Deposit amount must be positive.")
 
